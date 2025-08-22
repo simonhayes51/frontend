ee190da79a27bee4553796d08acfc441a87ab1d6
simonhayes51 <simonhayes_@hotmail.com>
2025-08-22 11:07:12 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        lime: '#00ff80',
+        limegreen: "#00ff80",
+        dark: "#0f0f0f",
+      },
+      fontFamily: {
+        sans: ["'Inter'", "sans-serif"],
       },
     },
   },
