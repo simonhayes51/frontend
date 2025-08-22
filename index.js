cd576079f191578a789934b2b04bd20a79a9d4a5
simonhayes51 <simonhayes_@hotmail.com>
2025-08-22 13:01:10 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
