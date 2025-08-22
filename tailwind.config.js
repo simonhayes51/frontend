fa495d432b349d5ea936fe53b7c96b786695a635
simonhayes51 <simonhayes_@hotmail.com>
2025-08-22 10:44:36 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        lime: '#00ff80',
+      },
+    },
+  },
+  plugins: [],
+}
