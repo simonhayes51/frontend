7af428f123e09912c3513cd30a69cce6568e57eb
simonhayes51 <simonhayes_@hotmail.com>
2025-08-22 02:08:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080
-    host: 0.0.0.0
-  }
-})
+    port: 8080,
+    host: '0.0.0.0'
