742be0b966d49ac2ee098cbfe0cb8f03e9b4b39a
simonhayes51 <simonhayes_@hotmail.com>
2025-08-22 10:46:37 +0100

    Create AddTrade.jsx

---
+++ src/pages/AddTrade.jsx
@@ -0,0 +1,9 @@
+const AddTrade = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-lime mb-4">➕ Add Trade</h2>
+      <p>Form to log a new trade will go here.</p>
+    </div>
+  )
+}
+export default AddTrade
