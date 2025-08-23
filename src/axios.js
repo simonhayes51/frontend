aa4ed479b4c01587a022f011e0f0c917714cc64b
simonhayes51 <simonhayes_@hotmail.com>
2025-08-23 18:29:34 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -6,9 +6,12 @@ const instance = axios.create({
 
 // Automatically include user_id from localStorage in GET requests
 instance.interceptors.request.use((config) => {
-  const userId = localStorage.getItem("user_id")
-  if (userId && config.method === "get") {
-    config.params = { ...(config.params || {}), user_id: userId }
+  // Check if we're in a browser environment
+  if (typeof window !== 'undefined') {
+    const userId = localStorage.getItem("user_id")
+    if (userId && config.method === "get") {
+      config.params = { ...(config.params || {}), user_id: userId }
+    }
   }
   return config
 })
