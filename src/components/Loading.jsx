0fd0e88ce9d33079e4e6389c2d995adfa8ea98d4
simonhayes51 <simonhayes_@hotmail.com>
2025-08-23 22:50:18 +0100

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,4 +1,4 @@
-jsxconst Loading = () => {
+const Loading = () => {
   return (
     <div className="min-h-screen bg-black text-white flex items-center justify-center">
       <div className="text-center">
