ea5966d39b0cdb39fcbec931b8d0e79bf3f31d2d
simonhayes51 <simonhayes_@hotmail.com>
2025-08-24 11:18:28 +0100

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,7 +11,7 @@ function Layout() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-purple-900 to-gray-900">
       {!isMobile && (
-        <div className="hidden md:block fixed left-0 top-0 h-screen">
+        <div className="hidden md:block fixed left-0 top-0 h-screen z-40">
           <DesktopSidebar />
         </div>
       )}
