984ee2d92876ba21afeb32c01216534f135395e9
simonhayes51 <simonhayes_@hotmail.com>
2025-08-24 08:16:54 +0100

    Update useMediaQuery.js

--- src/hooks/useMediaQuery.js
+++ src/hooks/useMediaQuery.js
@@ -17,31 +17,3 @@ export const useMediaQuery = (query) => {
 
   return matches;
 };
-
-// public/sw.js (Service Worker)
-const CACHE_NAME = 'fut-dashboard-v1';
-const urlsToCache = [
-  '/',
-  '/static/js/bundle.js',
-  '/static/css/main.css',
-  '/manifest.json'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
