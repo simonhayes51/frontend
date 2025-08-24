4cd6339cd152a036cee3f3d17aa0012bc8097278
simonhayes51 <simonhayes_@hotmail.com>
2025-08-24 22:44:40 +0100

    Update PriceCheck.jsx

--- src/pages/PriceCheck.jsx
+++ src/pages/PriceCheck.jsx
@@ -22,7 +22,7 @@ export default function PriceCheck() {
       // Fetch player price data
       const res = await axios.get(`/api/pricecheck`, {
         params: {
-          player: `${player.name} ${player.rating}`, // ✅ change player_name → player
+          player: `${player.name} ${player.rating}`,
           platform: "console",
         },
       });
