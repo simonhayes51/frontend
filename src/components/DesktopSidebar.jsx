24872fe5325c8719fc16a7a5264fc4dc1353466a
simonhayes51 <simonhayes_@hotmail.com>
2025-08-25 12:11:54 +0100

    Update DesktopSidebar.jsx

--- src/components/DesktopSidebar.jsx
+++ src/components/DesktopSidebar.jsx
@@ -43,7 +43,7 @@ const DesktopSidebar = () => {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
       )
-    }
+    },
     { 
       path: '/analytics', 
       label: 'Analytics',
