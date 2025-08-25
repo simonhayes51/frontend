61075b914a1e769a6859d5b80ec130fac0882296
simonhayes51 <simonhayes_@hotmail.com>
2025-08-25 11:42:29 +0100

    Create PlayerSearch.jsx

---
+++ src/pages/PlayerSearch.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import FutPlayerSearch from '../components/FutPlayerSearch';
+
+const PlayerSearch = () => {
+  return <FutPlayerSearch />;
+};
+
+export default PlayerSearch;
